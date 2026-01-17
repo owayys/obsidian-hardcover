ff16e6aa2bdd5e8312b65251857eb19aaa6fb56e
owayys <owais.ahsan@hotmail.com>
2026-01-17 21:40:16 +0500

    fix linting

--- codegen.ts
+++ codegen.ts
@@ -1,41 +1,41 @@
-import type { CodegenConfig } from "@graphql-codegen/cli";
+import type { CodegenConfig } from "@graphql-codegen/cli"
 
 const config: CodegenConfig = {
-	overwrite: true,
-	schema: {
-		"https://api.hardcover.app/v1/graphql": {
-			headers: {
-				authorization: `Bearer ${process.env.HARDCOVER_AUTH_TOKEN}`,
-			},
-		},
-	},
-	documents: "src/api/operations/**/*.graphql",
-	generates: {
-		"src/api/client/": {
-			preset: "client",
-			config: {
-				documentMode: "string",
-				scalars: {
-					date: "string",
-					timestamptz: "string",
-					timestamp: "string",
-					bigint: "number",
-					citext: "string",
-					float8: "number",
-					json: "Record<string, any>",
-					jsonb: "Record<string, any>",
-					numeric: "number",
-					smallint: "number",
-				},
-			},
-		},
-		"src/api/schema.graphql": {
-			plugins: ["schema-ast"],
-			config: {
-				includeDirectives: true,
-			},
-		},
-	},
-};
+  overwrite: true,
+  schema: {
+    "https://api.hardcover.app/v1/graphql": {
+      headers: {
+        authorization: `Bearer ${process.env.HARDCOVER_AUTH_TOKEN}`,
+      },
+    },
+  },
+  documents: "src/api/operations/**/*.graphql",
+  generates: {
+    "src/api/client/": {
+      preset: "client",
+      config: {
+        documentMode: "string",
+        scalars: {
+          date: "string",
+          timestamptz: "string",
+          timestamp: "string",
+          bigint: "number",
+          citext: "string",
+          float8: "number",
+          json: "Record<string, any>",
+          jsonb: "Record<string, any>",
+          numeric: "number",
+          smallint: "number",
+        },
+      },
+    },
+    "src/api/schema.graphql": {
+      plugins: ["schema-ast"],
+      config: {
+        includeDirectives: true,
+      },
+    },
+  },
+}
 
-export default config;
+export default config
